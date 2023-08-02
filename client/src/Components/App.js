2ff0d675ef052897569191a286647361a7073ebc
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-08-02 09:26:30 -0400

    react router

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
 
 function App() {
   return (
