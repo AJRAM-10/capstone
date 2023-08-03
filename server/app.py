5972740015043aa8e62dc3bd56ecc6af8542d020
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-08-03 14:33:25 -0400

    flask bycrypt

--- server/app.py
+++ server/app.py
@@ -119,6 +119,44 @@ class UserById(Resource):
 api.add_resource(UserById, '/users/<int:id>')
 
 
+class Login(Resource):
+
+    def get(self):
+        ...
+
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return user.to_dict()
+
+api.add_resource(Login, '/login')
+
+
+
+class Logout(Resource):
+
+    def delete(self): # just add this line!
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
